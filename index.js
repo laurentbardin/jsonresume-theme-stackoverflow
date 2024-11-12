9b44848c4fbadaf0b2a7bb4169d3c4c4270df576
Laurent Bardin <laurent.bardin+gitlab@protonmail.com>
2024-11-12 21:16:45 +0100

    Simplify education dates display logic

--- index.js
+++ index.js
@@ -35,8 +35,6 @@ function render(resume) {
     Handlebars.registerPartial(name, template);
   });
 
-  checkDates(resume.education)
-
   return Handlebars.compile(tpl)({
     css,
     resume,
@@ -51,16 +49,6 @@ const pdfRenderOptions = {
     left: marginValue,
     right: marginValue,
   }
-}
-
-function checkDates(data) {
-  data.forEach(function (elt, id, d) {
-    if (elt.startDate && elt.endDate) {
-      d[id].showDates = true
-    } else {
-      d[id].showDates = false
-    }
-  })
 }
 
 module.exports = { render, pdfRenderOptions };

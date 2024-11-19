e4d8fb778a48376d44f262bcd6e3eaf499631bd8
Laurent Bardin <laurent.bardin+gitlab@protonmail.com>
2024-11-19 12:42:41 +0100

    Localize dates correctly

--- theme/hbs-helpers/date-helpers.js
+++ theme/hbs-helpers/date-helpers.js
@@ -1,15 +1,9 @@
 const moment = require('moment');
 
 const dateHelpers = {
-  MY: function(date, locale) {
-      moment.locale(locale);
-      return moment(date.toString(), ['YYYY-MM-DD']).format('MMM YYYY');
-  },
+  MY: date => moment(date.toString(), ['YYYY-MM-DD']).format('MMM YYYY'),
   Y: date => moment(date.toString(), ['YYYY-MM-DD']).format('YYYY'),
-  DMY: function (date, locale) {
-      moment.locale(locale);
-      return moment(date.toString(), ['YYYY-MM-DD']).format('D MMM YYYY');
-  }
+  DMY: date => moment(date.toString(), ['YYYY-MM-DD']).format('D MMM YYYY')
 };
 
 module.exports = { dateHelpers };
